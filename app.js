5b3fff999044f34b64b086ccdc6d66e15ea1566a
Matt Dillon <mddillon47@gmail.com>
2021-04-04 22:13:50 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log("Let's get this party started!");
+
+const $form = $(".form-control");
+const $input = $("#search");
+const $button = $("#remove");
+
+function addGif(res) {
+    
+}
+
+$form.on("submit", async function getGiphy(e) {
+e.preventDefault;
+const userInput = $input.val();
+$input.val("")
+
+  const res = await axios.get("http://api.giphy.com/v1/gifs/search", {
+    params: { q: userInput, api_key: "5kqOJ0P9BZz3hE8pgl9dkZwb0xD0CWDy" },
+  });
+  addGif(res);
+})
+
+function removePicture() {}
